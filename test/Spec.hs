e892d10ff7b8cf80ffcfd06383d5fee97fd40373
David Luposchainsky <dluposchainsky@gmail.com>
2016-03-01 19:38:44 +0100

    Move test descriptions to their test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main (main) where
+
+
 
 import Test.QuickCheck
 import Test.Tasty
@@ -8,7 +12,9 @@ import qualified Data.Vector as V
 import qualified Data.Vector.Algorithms.Merge as Merge
 import qualified Data.Vector.Algorithms.Heap as Heap
 
-import Sorting
+import qualified Sorting as Subject
+
+
 
 main :: IO ()
 main = do
@@ -19,18 +25,26 @@ main = do
     defaultMain . quickcheckOptions $
         testGroup "Sorting"
             [ testGroup "Quicksort"
-                [ testProperty "agrees with library's Heapsort" prop_quicksort_heapsort
-                , testProperty "agrees with library's Mergesort" prop_quicksort_mergesort
+                [ test prop_quicksort_heapsort
+                , test prop_quicksort_mergesort
                 ]
             , testGroup "Slowsort"
-                [ testProperty "agrees with library's Heapsort" prop_slowsort_heapsort
-                , testProperty "agrees with library's Mergesort" prop_slowsort_mergesort
+                [ test prop_slowsort_heapsort
+                , test prop_slowsort_mergesort
                 ]
             , testGroup "Selection sort"
-                [ testProperty "agrees with library's Heapsort" prop_selectionsort_heapsort
-                , testProperty "agrees with library's Mergesort" prop_selectionsort_mergesort
+                [ test prop_selectionsort_heapsort
+                , test prop_selectionsort_mergesort
                 ]
             ]
+
+
+
+-- | Test case grouped with a descriptive name.
+data Testcase a = Test String a
+
+test :: Testable a => Testcase a -> TestTree
+test (Test descr prop) = testProperty descr prop
 
 
 
@@ -43,15 +57,34 @@ denotationallyEqual
 denotationallyEqual f g = \xs -> let vec = V.fromList xs
                                  in f vec == g vec
 
-prop_quicksort_heapsort, prop_quicksort_mergesort :: [Int] -> Bool
-prop_quicksort_heapsort = denotationallyEqual quicksort (V.modify Heap.sort)
-prop_quicksort_mergesort = denotationallyEqual quicksort (V.modify Merge.sort)
+prop_quicksort_heapsort, prop_quicksort_mergesort :: Testcase ([Int] -> Bool)
+prop_quicksort_heapsort = Test
+    "agrees with library's heapsort"
+    (denotationallyEqual Subject.quicksort (V.modify Heap.sort))
+prop_quicksort_mergesort = Test
+    "agrees with library's mergesort"
+    (denotationallyEqual Subject.quicksort (V.modify Merge.sort))
 
-prop_slowsort_heapsort, prop_slowsort_mergesort :: [Int] -> Bool
--- take 50 so slowsort doesn't slow down the tests too much :-)
-prop_slowsort_heapsort = denotationallyEqual slowsort (V.modify Heap.sort) . take 20
-prop_slowsort_mergesort = denotationallyEqual slowsort (V.modify Merge.sort) . take 20
 
-prop_selectionsort_heapsort, prop_selectionsort_mergesort :: [Int] -> Bool
-prop_selectionsort_heapsort = denotationallyEqual selectionsort (V.modify Heap.sort)
-prop_selectionsort_mergesort = denotationallyEqual selectionsort (V.modify Merge.sort)
+prop_slowsort_heapsort, prop_slowsort_mergesort :: Testcase Property
+-- Test generators are scaled down so this won't dominate the test time taken
+prop_slowsort_heapsort = Test
+    "agrees with library's heapsort"
+    (forAll (scale (min 32) arbitrary)
+            (\xs -> let _ = xs :: [Int]
+                    in denotationallyEqual Subject.slowsort (V.modify Heap.sort) xs))
+prop_slowsort_mergesort = Test
+    "agrees with library's mergesort"
+    (forAll (scale (min 32) arbitrary)
+            (\xs -> let _ = xs :: [Int]
+                    in denotationallyEqual Subject.slowsort (V.modify Merge.sort) xs))
+-- take 32 so slowsort doesn't slow down the tests too much :-)
+
+
+prop_selectionsort_heapsort, prop_selectionsort_mergesort :: Testcase ([Int] -> Bool)
+prop_selectionsort_heapsort = Test
+    "agrees with library's heapsort"
+    (denotationallyEqual Subject.selectionsort (V.modify Heap.sort))
+prop_selectionsort_mergesort = Test
+    "agrees with library's mergesort"
+    (denotationallyEqual Subject.selectionsort (V.modify Merge.sort))
